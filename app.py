83576d6ee5b3597c73faee7fdafe18e174949c33
max <max@gmail.com>
2018-10-17 20:43:57 +0300

    Add broken JS

--- app.py
+++ app.py
@@ -12,18 +12,18 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/search_results', methods=['POST'])
+@app.route('/results', methods=['GET'])
 def results():
-    if request.method == 'POST' and request.form['plc']:
-        best_rests = set_best_rest_list(gmaps, request.form['plc'])
-        return render_template('results.html', rest_list=best_rests)
+    if request.method == 'GET' and request.values['plc']:
+        best_rests = set_best_rest_list(gmaps, request.values['plc'])
+        return render_template('home.html', rest_list=best_rests)
     else:
         return render_template('home.html')
 
-
-@app.route('/search_rest', methods=['POST', 'GET'])
-def get_rest():
-    return get_rest_list(gmaps, request.values['plc'])
+#
+# @app.route('/search_rest', methods=['POST', 'GET'])
+# def get_rest():
+#     return get_rest_list(gmaps, request.values['plc'])
 
 
 if __name__ == '__main__':
